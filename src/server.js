05d4cc550602f1bf1aaa1617df812cb77fc004e5
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-04-11 14:55:14 -0300

    Estruturando Node + Express

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./router');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../', 'public')));
+app.use(routes);
+
+module.exports = app;
